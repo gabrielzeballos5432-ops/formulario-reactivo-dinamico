b803a76e81173ae86fbbd4ad69586eba66eaef93
jgabriel2005 <zeballosgabriel52@gmail.com>
2026-03-02 11:53:44 -0400

    Formulario reactivo en funcionamiento

--- formulario-reactivo-dinamico/src/app/app.component.ts
+++ formulario-reactivo-dinamico/src/app/app.component.ts
@@ -8,6 +8,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('formulario_reactivo');
 }
--- formulario-reactivo-dinamico/src/app/app.routes.ts
+++ formulario-reactivo-dinamico/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: FormularioComponent
+  }
+];
